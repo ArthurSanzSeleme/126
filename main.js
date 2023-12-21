561b79e80b0939e285a5ce1cccfc005186396094
ArthurSanzSeleme <133713885+ArthurSanzSeleme@users.noreply.github.com>
2023-12-21 15:55:59 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
-song = "";
+song1 = "";
+song2 = "";
 
-function preload()
-{
-	song = loadSound("music.mp3");
-}
+song1_status = "";
+song2_status = "";
 
 scoreRightWrist = 0;
 scoreLeftWrist = 0;
@@ -13,6 +12,12 @@ rightWristY = 0;
 
 leftWristX = 0;
 leftWristY = 0;
+
+function preload()
+{
+	song1 = loadSound("music.mp3");
+	song2 = loadSound("music2.mp3");
+}
 
 function setup() {
 	canvas =  createCanvas(600, 500);
@@ -36,7 +41,7 @@ function gotPoses(results)
 	console.log(results);
 	scoreRightWrist =  results[0].pose.keypoints[10].score;
 	scoreLeftWrist =  results[0].pose.keypoints[9].score;
-	console.log("scoreRightWrist = " + scoreRightWrist + " scoreLeftWrist = " + scoreLeftWrist);
+	console.log("scoreRightWrist = " + scoreRightWrist + "scoreLeftWrist = " + scoreLeftWrist);
 	
 	rightWristX = results[0].pose.rightWrist.x;
 	rightWristY = results[0].pose.rightWrist.y;
@@ -51,6 +56,9 @@ function gotPoses(results)
 
 function draw() {
 	image(video, 0, 0, 600, 500);
+	
+	song1_status = song1.isPlaying();
+	song2_status = song2.isPlaying();
 
 	fill("#FF0000");
 	stroke("#FF0000");
@@ -59,41 +67,26 @@ function draw() {
 	{ 
 		circle(rightWristX,rightWristY,20);
 
-		if(rightWristY >0 && rightWristY <= 100)
+			song2.stop();
+
+		if(song1_status == false)
 		{
-			document.getElementById("speed").innerHTML = "Velocidade = 0.5x";		
-			song.rate(0.5);
-		}
-		else if(rightWristY >100 && rightWristY <= 200)
-		{
-			document.getElementById("speed").innerHTML = "Velocidade = 1x";		
-			song.rate(1);
-		}
-		else if(rightWristY >200 && rightWristY <= 300)
-		{
-			document.getElementById("speed").innerHTML = "Velocidade = 1.5x";		
-			song.rate(1.5);
-		}
-		else if(rightWristY >300 && rightWristY <= 400)
-		{
-			document.getElementById("speed").innerHTML = "Velocidade = 2x";		
-			song.rate(2);
-		}
-		else if(rightWristY >400)
-		{
-			document.getElementById("speed").innerHTML = "Velocidade = 2.5x";		
-			song.rate(2.5);
+			song1.play();
+			document.getElementById("song").innerHTML = "Tocando: Tema de Harry Potter"
 		}
 	}
 
 	if(scoreLeftWrist > 0.2)
 	{
 		circle(leftWristX,leftWristY,20);
-		InNumberleftWristY = Number(leftWristY); 
-		remove_decimals = floor(InNumberleftWristY);
-		volume = remove_decimals/500;
-		document.getElementById("volume").innerHTML = "Volume = " + volume;		
-		song.setVolume(volume);	
+
+			song1.stop();
+
+		if(song2_status == false)
+		{
+			song2.play();
+			document.getElementById("song").innerHTML = "Tocando: Música de Peter Pan"
+		}
 	}
 
 }
@@ -104,3 +97,10 @@ function play()
 	song.setVolume(1);
 	song.rate(1);
 }
+
+
+
+
+
+
+
